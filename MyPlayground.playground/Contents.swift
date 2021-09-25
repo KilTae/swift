ed95e3cdd77a395ff1711b4d0268113cc50159cb
KilTae <loverpleasestayo0@gmail.com>
2021-09-25 16:03:44 +0900

    feat:swift 문법

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,29 @@
+var sexy=2000; //var 는 변수를 바꿀 수 있음. 하지만 let은 변수를 바꾸지 못한다. 그런데 null값으로 두고 바꾸는 건 가능.
+sexy=1000;
+sexy=20202;
+sexy;
+let meterToFeet=20.3;
+let year=1999;
+var x: String="Korea"; // : String int 변수 선언을 할 수 있음.
+
+
+var account = 100;
+account=account-10;
+print(45);
+
+
+//if
+
+var age=19;
+if age > 17 {
+    print("You are able to get driver license");
+}else{
+    print ("You are too young to get license");
+}
+
+var sound=70;
+if sound >= 85{
+    print("too loud");
+}else{
+    print("safe");
+}
