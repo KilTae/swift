35474ec4e8ec8c71e898ebe64dd67177606c94df
KilTae <loverpleasestayo0@gmail.com>
2022-01-22 15:41:20 +0900

    [feat] url 이미지

--- 2m4t_home/2m4t_home/ImageCollectionViewCell.swift
+++ 2m4t_home/2m4t_home/ImageCollectionViewCell.swift
@@ -11,12 +11,28 @@ class ImageCollectionViewCell: UICollectionViewCell {
     
     static let identifier : String = "ImageCollectionViewCell"
     @IBOutlet weak var ImageView: UIImageView!
-    
+    var noserviceClosure : (()->())?
+  
     func setData(imageName : String){
         
-        if let image = UIImage(named: imageName){
-            ImageView.image = image
+//        if let image = UIImage(named: imageName){
+//            ImageView.image = image
+//        }
+        
+        let url = URL(string: "https://image.tmdb.org/t/p/original\(imageName)")
+        DispatchQueue.global().async {
+            let data = try? Data(contentsOf: url!)
+            DispatchQueue.main.async {
+                self.ImageView.image = UIImage(data : data!)
+            }
         }
+                           
+     
+        
+        
+        
+        
+        
     }
     
     
